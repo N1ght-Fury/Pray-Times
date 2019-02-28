2fc1daaa413a3dc5cfa96ddc46711e7bbd6e9ad1
N1ght-Fury <44553433+N1ght-Fury@users.noreply.github.com>
2019-02-28 22:48:08 +0300

    Update Pray_Times.py

--- Pray_Times.py
+++ Pray_Times.py
@@ -12,39 +12,47 @@ def inform_user(current_secs, next_time, name_of_time):
 	print("Remaining Time:", time_left)
 
 def main():
-	url = "https://www.sabah.com.tr/json/getpraytimes/istanbul?dayafter=0"
-	response = requests.get(url)
-	data = str(response.content)
+	for i in range(2):
+		print(i)
+		url = "https://www.sabah.com.tr/json/getpraytimes/istanbul?dayafter=" + str(i)
+		response = requests.get(url)
+		data = str(response.content)
 
-	data = data[2:]
-	data = data.replace("xc4","").replace("xb0","")
-	data = data[:-1]
+		data = data[2:]
+		data = data.replace("xc4","").replace("xb0","")
+		data = data[:-1]
 
-	data = json.loads(data,encoding = "UTF-8")
+		data = json.loads(data,encoding = "UTF-8")
 
-	Imsak = int(data['List'][0]['Imsak'].replace("Date","").replace("(","").replace(")","").replace("/","")[:-1][1:]) / 1000
-	Gunes = int(data['List'][0]['Gunes'].replace("Date","").replace("(","").replace(")","").replace("/","")[:-1][1:]) / 1000
-	Ogle = int(data['List'][0]['Ogle'].replace("Date","").replace("(","").replace(")","").replace("/","")[:-1][1:]) / 1000
-	Ikindi = int(data['List'][0]['Ikindi'].replace("Date","").replace("(","").replace(")","").replace("/","")[:-1][1:]) / 1000
-	Aksam = int(data['List'][0]['Aksam'].replace("Date","").replace("(","").replace(")","").replace("/","")[:-1][1:]) / 1000  
-	Yatsi = int(data['List'][0]['Yatsi'].replace("Date","").replace("(","").replace(")","").replace("/","")[:-1][1:]) / 1000
+		Imsak = int(data['List'][i]['Imsak'].replace("Date","").replace("(","").replace(")","").replace("/","")[:-1][1:]) / 1000
+		Gunes = int(data['List'][i]['Gunes'].replace("Date","").replace("(","").replace(")","").replace("/","")[:-1][1:]) / 1000
+		Ogle = int(data['List'][i]['Ogle'].replace("Date","").replace("(","").replace(")","").replace("/","")[:-1][1:]) / 1000
+		Ikindi = int(data['List'][i]['Ikindi'].replace("Date","").replace("(","").replace(")","").replace("/","")[:-1][1:]) / 1000
+		Aksam = int(data['List'][i]['Aksam'].replace("Date","").replace("(","").replace(")","").replace("/","")[:-1][1:]) / 1000  
+		Yatsi = int(data['List'][i]['Yatsi'].replace("Date","").replace("(","").replace(")","").replace("/","")[:-1][1:]) / 1000
 
-	current_time = datetime.now()
-	in_secs = datetime.timestamp(current_time)
-	difference = 0
+		current_time = datetime.now()
+		in_secs = datetime.timestamp(current_time)
+		difference = 0
 
-	if (in_secs < Imsak):
-		inform_user(in_secs, Imsak, "Imsak")
-	elif (in_secs < Gunes):
-		inform_user(in_secs, Gunes, "Gunes")
-	elif (in_secs < Ogle):
-		inform_user(in_secs, Ogle, "Ogle")
-	elif (in_secs < Ikindi):
-		inform_user(in_secs, Ikindi, "Ikindi")
-	elif (in_secs < Aksam):
-		inform_user(in_secs, Aksam, "Aksam")
-	elif (in_secs < Yatsi):
-		inform_user(in_secs, Yatsi, "Yatsi")
+		if (in_secs < Imsak):
+			inform_user(in_secs, Imsak, "Imsak")
+			return 0
+		elif (in_secs < Gunes):
+			inform_user(in_secs, Gunes, "Gunes")
+			return 0
+		elif (in_secs < Ogle):
+			inform_user(in_secs, Ogle, "Ogle")
+			return 0
+		elif (in_secs < Ikindi):
+			inform_user(in_secs, Ikindi, "Ikindi")
+			return 0
+		elif (in_secs < Aksam):
+			inform_user(in_secs, Aksam, "Aksam")
+			return 0
+		elif (in_secs < Yatsi):
+			inform_user(in_secs, Yatsi, "Yatsi")
+			return 0
 
 if __name__ == "__main__":
 	main()
